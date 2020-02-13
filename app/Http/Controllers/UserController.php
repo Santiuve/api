005aece2c109a015dd8d0c9e7384267a06f3611c
Soporte Sistemas <soporte@grupofs-fsc.local>
2020-02-13 17:18:50 -0500

    ok

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function pruebas(Request $request){
+        return "Acción de pruebas de USER-CONTROLLER";
+    }
+    public function register(Request $request){
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+        if(!empty($params) && !empty($params_array)){
+            $params_array = array_map('trim', $params_array);
+            $validate = \Validator::make($params_array, [
+                'name' => 'required|alpha',
+                'surname' => 'required|alpha',
+                'email' => 'required|email|unique:users',
+                'password' => 'required'
+            ]);
+            if($validate->fails()){
+                $data = array(
+                    'status' => 'error',
+                    'code' => 404,
+                    'message' => 'El usuario no se ha creado',
+                    'errors' => $validate->errors()
+                );
+            }else{
+                $pwd = hash('sha256', $params->password);
+                $user = new User();
+                $user->name = $params_array['name']; 
+                $user->surname = $params_array['surname']; 
+                $user->email = $params_array['email']; 
+                $user->password = $pwd;
+                $user->role = 'ROLE_USER';
+                $user->save();
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'El usuario se ha creado correctamente',
+                    'user' => $user
+                );
+            }
+        }else{
+            $data = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Los datos enviados no son correctos'
+            );
+        }
+        return response()->json($data, $data['code']);
+    }
+    public function login(Request $request){
+        $jwtAuth = new \JwtAuth();
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+        $validate = \Validator::make($params_array, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if($validate->fails()){
+            $signup = array(
+                'status' => 'error',
+                'code' => '404',
+                'message' => 'El usuario no se ha podido identificar',
+                'errors' => $validate->errors()
+            );
+        }else{
+            $pwd = hash('sha256', $params->password);
+            $signup = $jwtAuth->signup($params->email, $pwd);
+            if(!empty($params->getToken)){
+                $signup = $jwtAuth->signup($params->email, $pwd, true);
+            }
+        }
+        $email = 'omar_23_co@hotmail.com';
+        $password = 'omar231198';
+        $pwd = hash('sha256', $password);
+        return response()->json($signup, 200);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return '<h1>Hola Mundo</h1>';
+});
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/pruebas/{nombre?}', function ($nombre = null) {
+    $texto = '<h2>Texto desde una ruta</h2>';
+    $texto.= 'Nombre: '.$nombre;
+    return view('pruebas', array(
+        'texto' => $texto
+    ));
+});
+Route::get('/animales', 'PruebasController@index');
+Route::get('/test-orm', 'PruebasController@testOrm');
+Route::get('/usuario/pruebas', 'UserController@pruebas');
+Route::get('/categoria/pruebas', 'CategoryController@pruebas');
+Route::get('/entrada/pruebas', 'PostController@pruebas');
+Route::post('/api/register', 'UserController@register');
+Route::post('/api/login', 'UserController@login');
